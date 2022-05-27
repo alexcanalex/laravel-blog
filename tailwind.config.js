6b60a22dfc2d762c485b27388343dc0ef99a94c6
Alejandro Canales <82683880+alexcanalex@users.noreply.github.com>
2022-05-26 20:36:33 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    content: [
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    },
+
+    plugins: [require('@tailwindcss/ui')],
+
+    corePlugins: {
+        // ...
+        container: false,
+    }
+};
